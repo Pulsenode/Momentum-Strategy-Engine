de7bbc182ecc73ff61acd58bbe973da17f23303c
Clement Dedieu <dedieuclementpro@gmail.com>
2026-01-12 21:17:07 +1000

    Refactor stock analysis function and enhance API data retrieval

--- src/index.js
+++ src/index.js
@@ -1,5 +1,46 @@
+//https://financialmodelingprep.com/stable/search-symbol?query=AAPL&apikey=RX4VBBM2yKP35M35NbHPNqkdwBADjd4e
+
 require ('dotenv').config();
 
-const maCle = process.env.API_KEY;
-console.log("Ma clé est chargée en toute sécurité !"+ maCle);
+const API_KEY = process.env.API_KEY;
+const BASE_URL = "https://financialmodelingprep.com/api/v3/";
+
+
+async function demarrerAnalyse() {
+console.log("Stock Analysis starting");
+
+const sp500 = await API_GET("sp500_constituent", "");
+  
+  if (sp500) {
+    // On garde les 100 premiers pour ton analyse de Momentum
+    const top100 = sp500.slice(0, 100);
+    
+    console.log(`Liste récupérée ! Nombre d'actions : ${sp500.length}`);
+    console.log("Analyse du Top 100 lancée...");
+    
+    // C'est ici que tu feras ta boucle plus tard
+    console.log("Première action du top :", top100[0].symbol); 
+  }
+}
+
+demarrerAnalyse();
+
+
+async function API_GET(endpoint, queryParam = "") {
+  try {
+    const separator = queryParam ? "/" : "";
+
+    const url = `${BASE_URL}${endpoint}${separator}?${queryParam}&apikey=${API_KEY}`;
+
+    const response = await fetch(url);
+    if (!response.ok) {throw new Error(`HTTP error! status: ${response.status}`)};
+
+    return await response.json();
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error.message);
+    return null;
+  }
+}
+
+
 
